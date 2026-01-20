30bd8b9eb32af76a18fab77080f4760c030c7277
Zayneb Bellil <zainebbellil14@gmail.com>
2026-01-20 12:11:48 +0100

    feat: add simple homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Link } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 function HomePage() {
-	return <div></div>;
+	return (
+		<div className="min-h-screen flex justify-center items-center">
+			<Link to="/login">
+				<Button variant="secondary" className="cursor-pointer mx-2">
+					Go Login
+				</Button>
+			</Link>
+
+			<Link to="/register">
+				<Button className="cursor-pointer mx-2"> Go Register </Button>
+			</Link>
+		</div>
+	);
 }
 
 export default HomePage;
