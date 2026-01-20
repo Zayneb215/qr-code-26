c4a37b28f2d92c71c62f303eca80bbed65e43322
Zayneb215 <zainebbellil14@gmail.com>
2026-01-20 22:17:04 +0100

    feat:create loginpage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,9 @@
 import { Label } from "@radix-ui/react-label";
+import { Link } from "@tanstack/react-router";
+import axios from "axios";
 import { useState } from "react";
 import Background from "@/components/Background";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
 	Card,
@@ -12,15 +15,38 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Link } from "@tanstack/react-router";
+import { API_BASE_URL } from "@/constants/api";
 
+interface LoginResponse {
+	access_token: string;
+}
 function LoginPage() {
-	const [email, setEmail] = useState<string>("");
-	const [password, setPassword] = useState<string>("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState<string | null>(null);
 
-	const login = () => {
-		console.log("Logging in with", { email, password });
-	};
+	async function login() {
+		try {
+			const data = new URLSearchParams();
+			data.append("username", email);
+			data.append("password", password);
+			const response = await axios.post<LoginResponse>(
+				`${API_BASE_URL}/auth/jwt/login`,
+				data,
+				{
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
+				},
+			);
+
+			localStorage.setItem("token", response.data.access_token);
+			console.log("LOGIN OK ✅", response.data);
+		} catch (err: any) {
+			setError(err.response?.data?.message || "Login failed");
+		}
+	}
+
 	return (
 		<Background>
 			<div className="min-h-screen flex justify-center items-center">
@@ -36,35 +62,38 @@ function LoginPage() {
 							</Link>
 						</CardAction>
 					</CardHeader>
+
 					<CardContent>
 						<form>
 							<div className="flex flex-col gap-6">
 								<div className="grid gap-2">
-									<Label htmlFor="email">Email</Label>
+									<Label>Email</Label>
 									<Input
 										type="email"
-										placeholder="m@example.com"
 										required
-										onChange={(event) => setEmail(event.target.value)}
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
 									/>
 								</div>
+
 								<div className="grid gap-2">
-									<div className="flex items-center">
-										<Label htmlFor="password">Password</Label>
-									</div>
+									<Label>Password</Label>
 									<Input
 										type="password"
 										required
-										onChange={(event) => setPassword(event.target.value)}
+										value={password}
+										onChange={(e) => setPassword(e.target.value)}
 									/>
 								</div>
 							</div>
 						</form>
 					</CardContent>
+
 					<CardFooter className="flex-col gap-2">
-						<Button type="submit" className="w-full" onClick={login}>
+						<Button type="submit" className="w-full">
 							Login
 						</Button>
+						{error && <Badge variant={"destructive"}>{error}</Badge>}
 					</CardFooter>
 				</Card>
 			</div>
