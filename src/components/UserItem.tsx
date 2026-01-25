5249e055cbc5ae7b86503e479546b804fb5ee2ca
Zayneb215 <zainebbellil14@gmail.com>
2026-01-26 00:35:48 +0100

    feat:add UserIitems

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import type { User } from "@/hooks/use-users";
 
-function UserItem() {
-	return <div>UserItem</div>;
+interface UserItemProps {
+	user: User;
+}
+function UserItem(props: UserItemProps) {
+	const { user } = props;
+	return (
+		<div key={user.id} className="bg-primary text-white p-3 rounded-md">
+			<h1>{user.email}</h1>
+			<h1>{user.name}</h1>
+		</div>
+	);
 }
 
 export default UserItem;
